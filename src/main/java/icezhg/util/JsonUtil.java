71892e11e5d04115661b3801dbe8ed5d0c817619
zhongjibing <zhongjibing@163.com>
2017-09-06 21:38:01 +0800

    add overloading

--- src/main/java/icezhg/util/JsonUtil.java
+++ src/main/java/icezhg/util/JsonUtil.java
@@ -1,6 +1,7 @@
 package icezhg.util;
 
 import java.io.IOException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 import com.fasterxml.jackson.core.JsonParser;
@@ -20,24 +21,61 @@ public final class JsonUtil {
 
     private static final Logger LOG = LoggerFactory.getLogger(JsonUtil.class);
 
-    private static final ObjectMapper objectMapper;
+    private static final ObjectMapper OBJECT_MAPPER;
 
     static {
-        objectMapper = new ObjectMapper();
-        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
-                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-                .configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true)
-                .registerModule(new SimpleModule());
-        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        OBJECT_MAPPER = ObjectMapperFactory.getObjectMapper();
     }
 
     private JsonUtil() {
     }
 
+    private static class StringUtil {
+
+        private static boolean isBlank(String str) {
+            if (str == null || str.length() == 0) {
+                return true;
+            }
+
+            for (int i = 0; i < str.length(); i++) {
+                if (!Character.isWhitespace(str.charAt(i))) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+
+    private static class ObjectMapperFactory {
+
+        private static ObjectMapper getObjectMapper() {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                    .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS)
+                    .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                    .configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true)
+                    .registerModule(new SimpleModule());
+            objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+            return objectMapper;
+        }
+    }
+
     public static String toString(Object obj) {
         String retJson = null;
         try {
+            retJson = OBJECT_MAPPER.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            LOG.error(e.getMessage(), e);
+        }
+        return retJson;
+    }
+
+    public static String toString(Object obj, DateFormat dateFormat) {
+        String retJson = null;
+        try {
+            ObjectMapper objectMapper = ObjectMapperFactory.getObjectMapper();
+            objectMapper.setDateFormat(dateFormat);
             retJson = objectMapper.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
             LOG.error(e.getMessage(), e);
@@ -46,27 +84,25 @@ public final class JsonUtil {
     }
 
     public static <T> T toBean(String json, Class<T> clazz) {
-        try {
-            if (!StringUtil.isBlank(json)) {
-                return objectMapper.readValue(json, clazz);
+        if (!StringUtil.isBlank(json)) {
+            try {
+                return OBJECT_MAPPER.readValue(json, clazz);
+            } catch (IOException e) {
+                LOG.error(e.getMessage(), e);
             }
-        } catch (IOException e) {
-            LOG.error(e.getMessage(), e);
         }
         return null;
     }
 
     public static <T> T toBean(String json, TypeReference<T> typeRef) {
-        try {
-            if (!StringUtil.isBlank(json)) {
-                return objectMapper.readValue(json, typeRef);
+        if (!StringUtil.isBlank(json)) {
+            try {
+                return OBJECT_MAPPER.readValue(json, typeRef);
+            } catch (IOException e) {
+                LOG.error(e.getMessage(), e);
             }
-        } catch (IOException e) {
-            System.out.println(e);
-            LOG.error(e.getMessage(), e);
         }
         return null;
     }
-
 
 }
